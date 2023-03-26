6618c5e328a30856c6bee612335f3e5b68dd7510
olaomar2000 <olashab300@gmail.com>
2023-03-26 15:49:45 +0300

    commit message

--- app/src/main/java/com/example/testmcc/MainActivity.kt
+++ app/src/main/java/com/example/testmcc/MainActivity.kt
@@ -1,11 +1,118 @@
 package com.example.testmcc
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var image: Uri? = null
+    private lateinit var analytics: FirebaseAnalytics
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
+        Firebase.auth.signOut()
+
+
+
+
+
+
+
+
+
+
+        analytics = Firebase.analytics
+        getRegToken()
+        screen_Track("MainActivity", "home")
+        Log.e("ola", "test")
+        btn_choose.setOnClickListener {
+            val i = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(i, 100)
+
+        }
+
+        var randomNumber = UUID.randomUUID().toString()
+        val storageRef = FirebaseStorage.getInstance()
+            .getReference("images")
+        btn_uplode.setOnClickListener {
+            val imageRef = storageRef.child("image" + randomNumber)
+            imageRef.putFile(image!!).addOnSuccessListener {
+                Log.d("TAG", "Success: ");
+            }.addOnFailureListener {
+                Log.d("TAG", "Success: ");
+            }
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            image = data!!.data
+            imageView.setImageURI(data.data)
+        }
+    }
+
+    fun select_content(id: String, name: String, contentType: String) {
+        analytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_ID, id)
+            param(FirebaseAnalytics.Param.ITEM_NAME, name)
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, contentType)
+        }
+    }
+
+    fun screen_Track(screenClass: String, ScreenName: String) {
+        analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, screenClass)
+            param(FirebaseAnalytics.Param.SCREEN_NAME, ScreenName)
+        }
+
+
+    }
+
+    fun custemEvent() {
+        analytics.logEvent("imageShare") {
+            param("nameIamge", "ola.png")
+            param("shareFrom", "MainActivity")
+
+        }
+
+    }
+
+    fun getRegToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e("ola",
+                    "Fetching FCM registration token failed",
+                    task.exception)
+                return@addOnCompleteListener
+            }
+            // Get new FCM registration token
+            val token = task.result
+            Log.e("token", token.toString())
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/testmcc/SingUpActivity.kt
@@ -0,0 +1,72 @@
+package com.example.testmcc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sing_up.*
+
+class SingUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+           updateUI()
+        }
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+
+        auth = Firebase.auth
+        var email = edt_email.text
+        var password = edt_password.text
+
+        btn_singup.setOnClickListener {
+            Log.e("ola", email.toString())
+            Log.e("ola", password.toString())
+            createNewAccount(email.toString(), password.toString())
+          //  createNewAccount(email.toString(), password.toString())
+
+        }
+
+    }
+
+    fun updateUI() {
+        var i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+
+    private fun createNewAccount(email: String, password: String) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("ola", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("ola", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+
+        }
+
+
+    }
+
+
